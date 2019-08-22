f23e08d41e11b7d7860a2a4d9b24372594407f1d
shikuan.xu <shikuan.xu@advance.ai>
2019-08-22 15:40:47 +0800

    update mysql client

--- client.go
+++ client.go
@@ -3,102 +3,122 @@ package dbclient
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/mitchellh/mapstructure"
+	"github.com/sirupsen/logrus"
 	"log"
+	"sync"
+	"time"
 )
 
-type ClientConfig struct {
-	dataSourceName string
-	maxIdleConns   int
-	maxOpenConns   int
+type ClientConf struct {
+	DataSourceName  string
+	DDLPath         string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+	Flyway          bool
+	db              *sql.DB
+	mu              sync.Mutex
 }
 
-var Client ClientConfig
-
-func InitialDBClient(dataSourceName string, maxIdleConns int, maxOpenConns int) {
-	log.Printf("initial db client. dataSourceName : %v ; maxIdleConns : %v ; maxOpenConns : %v", dataSourceName, maxIdleConns, maxOpenConns)
-	Client.dataSourceName = dataSourceName
-	Client.maxIdleConns = maxIdleConns
-	Client.maxOpenConns = maxOpenConns
+func NewMysqlClientConf(DataSourceName string) *ClientConf {
+	return &ClientConf{
+		DataSourceName:  DataSourceName,
+		MaxIdleConns:    5,
+		MaxOpenConns:    10,
+		ConnMaxLifetime: 24 * time.Hour,
+	}
 }
 
-func (client *ClientConfig) getConnection() (*sql.DB, error) {
-	db, err := sql.Open("mysql", client.dataSourceName)
+func (cc *ClientConf) SetMaxIdleConns(MaxIdleConns int) {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	cc.MaxIdleConns = MaxIdleConns
+}
+
+func (cc *ClientConf) SetMaxOpenConns(MaxOpenConns int) {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	cc.MaxOpenConns = MaxOpenConns
+}
+
+func (cc *ClientConf) SetConnMaxLifetime(SetConnMaxLifetime time.Duration) {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	cc.ConnMaxLifetime = SetConnMaxLifetime
+}
+
+func (cc *ClientConf) Initial() error {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	db, err := sql.Open("mysql", cc.DataSourceName)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	err = db.Ping()
 	if err != nil {
-		log.Println("ping mysql error.", err)
+		return err
+	}
+	cc.db = db
+	return nil
+}
+
+func (cc *ClientConf) GetDB() (*sql.DB, error) {
+	if err := cc.db.Ping(); err != nil {
+		logrus.Errorf("get connect error. %v", err)
 		return nil, err
 	}
-	//mysqlClient.pool.SetConnMaxLifetime(time.Duration(connMaxLifetime) * time.Second)
-	db.SetMaxIdleConns(client.maxIdleConns)
-	db.SetMaxOpenConns(client.maxOpenConns)
-	return db, nil
+	return cc.db, nil
 }
 
-func (client *ClientConfig) Insert(sql string, args ...interface{}) (int64, error) {
-	db, err := client.getConnection()
+type FieldFunc func(rows *sql.Rows) error
+
+func (cc *ClientConf) FindCustom(query string, fieldFunc FieldFunc, args ...interface{}) error {
+	db, err := cc.GetDB()
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
+		return err
 	}
-	defer db.Close()
-	stm, err := db.Prepare(sql)
+	rows, err := db.Query(query, args...)
 	if err != nil {
-		log.Println("prepare mysql error.", err)
-		return 0, err
+		return err
 	}
-	defer stm.Close()
-	result, err := stm.Exec(args...)
-	if err != nil {
-		log.Println("insert data error.", err)
-		return 0, err
+	defer rows.Close()
+	for rows.Next() {
+		err := fieldFunc(rows)
+		if err != nil {
+			return err
+		}
 	}
-	return result.LastInsertId()
+	return rows.Err()
 }
 
-func (client *ClientConfig) Update(sql string, args ...interface{}) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) FindMapFirst(sql string, args ...interface{}) (map[string]interface{}, error) {
+	array, err := cc.FindMapArray(sql, args...)
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
-	}
-	defer db.Close()
-	stm, err := db.Prepare(sql)
-	if err != nil {
-		log.Println("prepare mysql error.", err)
-		return 0, err
-	}
-	defer stm.Close()
-	result, err := stm.Exec(args...)
-	if err != nil {
-		log.Println("update data error.", err)
-		return 0, err
-	}
-	return result.RowsAffected()
-}
-
-func (client *ClientConfig) Find(sql string, args ...interface{}) ([]map[string]interface{}, error) {
-	db, err := client.getConnection()
-	if err != nil {
-		log.Println("mysql get connection error.", err)
 		return nil, err
 	}
-	defer db.Close()
+	if array == nil || len(array) == 0 {
+		return nil, nil
+	}
+	return array[0], nil
+}
+
+func (cc *ClientConf) FindMapArray(sql string, args ...interface{}) ([]map[string]interface{}, error) {
+	db, err := cc.GetDB()
+	if err != nil {
+		return nil, err
+	}
 	tx, err := db.Begin()
 	if err != nil {
 		log.Println("mysql client get transaction error.", err)
 		return nil, err
 	}
-	defer tx.Commit()
 	rows, err := tx.Query(sql, args...)
 	if err != nil {
 		log.Println("Query error.", err)
 		return nil, err
 	}
 	defer rows.Close()
-	//Read database columns
 	columns, err := rows.Columns()
 	if err != nil {
 		log.Println("rows.Columns() error.", err)
@@ -130,111 +150,92 @@ func (client *ClientConfig) Find(sql string, args ...interface{}) ([]map[string]
 	return results, nil
 }
 
-func (client *ClientConfig) FindOne(sql string, args ...interface{}) (map[string]interface{}, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) FindList(sql string, input interface{}, args ...interface{}) error {
+	results, err := cc.FindMapArray(sql, args...)
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return nil, err
+		return err
 	}
-	defer db.Close()
-	tx, err := db.Begin()
+	config := &mapstructure.DecoderConfig{
+		DecodeHook:       mapstructure.StringToTimeHookFunc("2006-01-02 15:04:05"),
+		WeaklyTypedInput: true,
+		Result:           input,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
-		log.Println("mysql client get transaction error.", err)
-		return nil, err
+		return err
 	}
-	defer tx.Commit()
-	rows, err := tx.Query(sql, args...)
+	err = decoder.Decode(results)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	defer rows.Close()
-	//读出查询出的列字段名
-	columns, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	//values是每个列的值，这里获取到byte里
-	values := make([][]byte, len(columns))
-	//query.Scan的参数，因为每次查询出来的列是不定长的，用len(cols)定住当次查询的长度
-	scans := make([]interface{}, len(columns))
-	//让每一行数据都填充到[][]byte里面
-	for i := range values {
-		scans[i] = &values[i]
-	}
-	//最后得到的map
-	for rows.Next() { //循环，让游标往下推
-		if err := rows.Scan(scans...); err != nil { //query.Scan查询出来的不定长值放到scans[i] = &values[i],也就是每行都放在values里
-			return nil, err
-		}
-		row := make(map[string]interface{}) //每行数据
-		for k, v := range values {          //每行数据是放在values里面，现在把它挪到row里
-			key := columns[k]
-			row[key] = string(v)
-		}
-		return row, nil
-	}
-	return nil, nil
+	return nil
 }
 
-type BatchCallback func(*sql.Tx) (int64, error)
-
-func (client *ClientConfig) BatchInsert(callback BatchCallback) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) FindListByDefaultConfig(sql string, input interface{}, config *mapstructure.DecoderConfig, args ...interface{}) error {
+	results, err := cc.FindMapArray(sql, args...)
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
+		return err
 	}
-	defer db.Close()
-	tx, err := db.Begin()
+	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
-		log.Println("mysql client get transaction error.", err)
-		return 0, err
+		return err
 	}
-	result, err := callback(tx)
+	err = decoder.Decode(results)
 	if err != nil {
-		log.Println("batch insert data error.", err)
-		return 0, err
+		return err
 	}
-	defer tx.Commit()
-	return result, nil
+	return nil
 }
 
-func (client *ClientConfig) BatchUpdate(callback BatchCallback) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) FindFirst(sql string, input interface{}, args ...interface{}) error {
+	result, err := cc.FindMapFirst(sql, args...)
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
+		return err
 	}
-	defer db.Close()
-	tx, err := db.Begin()
+	config := &mapstructure.DecoderConfig{
+		DecodeHook:       mapstructure.StringToTimeHookFunc("2006-01-02 15:04:05"),
+		WeaklyTypedInput: true,
+		Result:           input,
+	}
+	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
-		log.Println("mysql client get transaction error.", err)
-		return 0, err
+		return err
 	}
-	result, err := callback(tx)
+	err = decoder.Decode(result)
 	if err != nil {
-		log.Println("batch update data error.", err)
-		return 0, err
+		return err
 	}
-	defer tx.Commit()
-	return result, nil
+	return nil
 }
 
-func (client *ClientConfig) Count(sql string) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) FindFirstByDefaultConfig(sql string, input interface{}, config *mapstructure.DecoderConfig, args ...interface{}) error {
+	result, err := cc.FindMapFirst(sql, args...)
 	if err != nil {
-		log.Println("mysql get connection error.", err)
+		return err
+	}
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+	err = decoder.Decode(result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cc *ClientConf) Count(sql string, args ...interface{}) (int64, error) {
+	db, err := cc.GetDB()
+	if err != nil {
 		return 0, err
 	}
-	defer db.Close()
 	tx, err := db.Begin()
 	if err != nil {
 		log.Println("mysql client get connection error.", err)
 		return 0, err
 	}
-	defer tx.Commit()
 	var count int64
-	countErr := tx.QueryRow(sql).Scan(&count)
+	countErr := tx.QueryRow(sql, args...).Scan(&count)
 	if countErr != nil {
 		log.Println("Query count error.", err)
 		return 0, err
@@ -242,44 +243,79 @@ func (client *ClientConfig) Count(sql string) (int64, error) {
 	return count, nil
 }
 
-//return affected count
-func (client *ClientConfig) Delete(sql string, args ...interface{}) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) Insert(sql string, args ...interface{}) (int64, error) {
+	db, err := cc.GetDB()
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
+		return 0, nil
 	}
-	defer db.Close()
 	stm, err := db.Prepare(sql)
 	if err != nil {
-		log.Println("mysql client get connection error.", err)
+		logrus.Errorf("prepare mysql error. %v", err)
 		return 0, err
 	}
 	defer stm.Close()
 	result, err := stm.Exec(args...)
 	if err != nil {
-		log.Println("delete data error.", err)
+		logrus.Errorf("insert data error. %v", err)
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+type TransactionCallback func(*sql.Tx) error
+
+func (cc *ClientConf) Transaction(callback TransactionCallback) error {
+	db, err := cc.GetDB()
+	if err != nil {
+		return nil
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		logrus.Errorf("mysql client get transaction error. %v", err)
+		return err
+	}
+	err = callback(tx)
+	if err != nil {
+		logrus.Errorf("transaction data error. %v", err)
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (cc *ClientConf) Update(sql string, args ...interface{}) (int64, error) {
+	db, err := cc.GetDB()
+	if err != nil {
+		return 0, nil
+	}
+	stm, err := db.Prepare(sql)
+	if err != nil {
+		logrus.Errorf("prepare mysql error. %v", err)
+		return 0, err
+	}
+	defer stm.Close()
+	result, err := stm.Exec(args...)
+	if err != nil {
+		logrus.Errorf("update data error. %v", err)
 		return 0, err
 	}
 	return result.RowsAffected()
 }
 
-func (client *ClientConfig) DeleteByPrimaryKey(sql string, id int64) (int64, error) {
-	db, err := client.getConnection()
+func (cc *ClientConf) Delete(sql string, args ...interface{}) (int64, error) {
+	db, err := cc.GetDB()
 	if err != nil {
-		log.Println("mysql get connection error.", err)
-		return 0, err
+		return 0, nil
 	}
-	defer db.Close()
 	stm, err := db.Prepare(sql)
 	if err != nil {
-		log.Println("mysql client get connection error.", err)
+		logrus.Errorf("prepare mysql error. %v", err)
 		return 0, err
 	}
 	defer stm.Close()
-	result, err := stm.Exec(id)
+	result, err := stm.Exec(args...)
 	if err != nil {
-		log.Println("DeleteByPrimaryKey error.", err)
+		logrus.Errorf("delete data error. %v", err)
 		return 0, err
 	}
 	return result.RowsAffected()
